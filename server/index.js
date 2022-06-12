ffe0eb2442f0b0a4dc3f7af20a54607645faa6ef
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-06-12 15:05:12 +0530

    added db connection

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
-
+const colors = require('colors');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 const app = express();
+
+// Connect to database
+connectDB();
 
 app.use(
   '/graphql',
