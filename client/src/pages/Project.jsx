b2fe3293bbbc01ced357149efe351d2a2e48f9cf
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-06-24 10:29:47 +0530

    update project feature done

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,9 +1,10 @@
 import { Link, useParams } from 'react-router-dom';
-import Spinner from '../components/Spinner';
 import { useQuery } from '@apollo/client';
 import { GET_PROJECT } from '../queries/projectQueries';
+import Spinner from '../components/Spinner';
 import ClientInfo from '../components/ClientInfo';
 import DeleteProjectButton from '../components/DeleteProjectButton';
+import EditProjectForm from '../components/EditProjectForm';
 
 const Project = () => {
   const { id } = useParams();
@@ -27,6 +28,7 @@ const Project = () => {
           <h5 className="mt-3">Project Status</h5>
           <p className="lead">{data.project.status}</p>
           <ClientInfo client={data.project.client} />
+          <EditProjectForm project={data.project} />
           <DeleteProjectButton projectId={data.project.id} />
         </div>
       )}
